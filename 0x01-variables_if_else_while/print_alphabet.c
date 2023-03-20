8ce732415c443f7baf9ca51e08e8098fdd0c9d64
Owendrinkwater <owenowade@gmail.com>
2023-03-20 14:54:28 -0700

    preproccessor

---
+++ 0x01-variables_if_else_while/print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
