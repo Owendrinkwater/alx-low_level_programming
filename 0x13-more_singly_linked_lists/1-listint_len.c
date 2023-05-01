673840845184bdecdc3d10ce09d9b0d73dbdff23
Owendrinkwater <owenowade@gmail.com>
2023-05-01 12:53:10 -0700

    listlen

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a listint_t list.
+ *
+ * @h: a pointer to the head of the listint_t list.
+ *
+ * Return: the number of elements in the listint_t list.
+ */
+
+size_t listint_len(const listint_h *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
